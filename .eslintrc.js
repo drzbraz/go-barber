a922a6d68fd2335e054dccf64e3047ac2f835ebb
drzbraz <drz.braz@gmail.com>
2020-04-03 08:55:54 -0300

    Api gobaber

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'class-method-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
+  }
+};
---
+++ src/database/migrations/20200403022447-create-users.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  // eslint-disable-next-line arrow-parens
+  down: queryInterface => queryInterface.dropTable('users'),
+};
