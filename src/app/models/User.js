22e8a0d0bf68132ed79ed42030ae43a2c0b45264
drzbraz <drz.braz@gmail.com>
2020-06-24 22:32:56 -0300

    update appointment

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -17,6 +17,7 @@ class User extends Model {
     );
 
     this.addHook('beforeSave', async (user) => {
+      console.log('teste');
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
